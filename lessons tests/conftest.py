24144df18c03fce179d66f823ebcb300c8c19bb5
ok-google-1747 <40169779@mail.ru>
2023-09-23 13:42:30 +0300

    Задание к уроку 2

--- lessons tests/conftest.py
+++ lessons tests/conftest.py
@@ -3,12 +3,12 @@ from selene import browser
 
 
 @pytest.fixture()
-def open_browser():
+def open_browser(browser_size):
     browser.open('https://www.google.ru/')
 
 
 @pytest.fixture()
-def browser_size(open_browser):
+def browser_size():
     browser.config.window_width = 1200
 
     browser.config.window_height = 830
